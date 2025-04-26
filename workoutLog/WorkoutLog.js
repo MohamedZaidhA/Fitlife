0f2e461a0cb54af47a6ad12ee421bb2aae27a5ad
Mohamed Zaidh <231701032@rajalakshmi.edu.in>
2025-04-26 14:49:55 +0530

    Add files via upload

---
+++ workoutLog/WorkoutLog.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('workout-form');
+const workoutList = document.getElementById('workout-list');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const exercise = document.getElementById('exercise').value;
+  const sets = document.getElementById('sets').value;
+  const rep = document.getElementById('rep').value;
+
+  const listItem = document.createElement('li');
+  listItem.textContent = `${exercise} - ${rep} reps -  ${sets} sets`;
+
+  workoutList.appendChild(listItem);
+
+  // Clear input fields
+  form.reset();
+});
+
